5e4bfacbfa004c7bb87c3cdbbd40386e0335bfc6
hongxiaoya123 <75617850+hongxiaoya123@users.noreply.github.com>
2020-12-14 21:26:14 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,21 +1,59 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 import unittest
 import os
 from HTMLTestRunner import HTMLTestRunner
+suite = unittest.TestSuite()
 
-sutie = unittest.TestSuite()
+loader=unittest.defaultTestLoader
+case = loader.discover(os.getcwd(),pattern="test*.py")
+suite.addTest(case)
+with open("银行测试报告.html","w+",encoding="utf-8") as f:
+    a = HTMLTestRunner.HTMLTestRunner(
+        stream=f,
+        verbosity=1,
+        title="银行取钱操作",
+        description="第一次测试"
+    )
+    a.run(suite)
 
-loader =unittest.defaultTestLoader
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 我的163授权码：OKIADSGIYSTENULF，qq授权码:pxwctgopudmbdhif
 
-case = loader.discover(os.getcwd(), pattern="calc*.py")
 
-sutie.addTest(case)
+sender = '2075518412@qq.com'
+receivers = ['1020397800@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+mail_host = 'smtp.qq.com'
+password = 'nnzjlebctssadddd'
+tester = '贾生'
+# 创建一个带附件的实例
+message = MIMEMultipart()
+message['From'] = '2075518412@qq.com'
+message['To'] = Header("Jason的邮件", 'utf-8')
+subject = '第一轮银行的单元测试报告 -- [' + tester + ']'
+message['Subject'] = Header(subject, 'utf-8')
 
-f = open("计算器.html", "w+", encoding="utf-8")
-htmlrunner =HTMLTestRunner.HTMLTestRunner(
-    stream=f,
-    title ="计算器钱的测试报告",
-    description ="这是一个计算器的测试",
-    verbosity=1,
-)
+# 邮件正文内容
+message.attach(MIMEText('详情请见附件消息！', 'plain', 'utf-8'))
 
-htmlrunner.run(sutie)
+# 构造附件1，传送当前目录下的 test.txt 文件
+f = open('银行测试报告.html', 'r+',encoding="utf-8").read() # 先读取报告文件
+att1 = MIMEText(f, 'base64', 'utf-8')  # 添加到附件里
+att1["Content-Type"] = 'application/octet-stream' # 添加属性头
+# 这里的filename可以任意写，写什么名字，邮件中显示什么名字
+att1["Content-Disposition"] = 'attachment; filename="bank_reporting.html"'
+message.attach(att1)
+
+try:
+    smtpObj = smtplib.SMTP()
+    smtpObj.connect(mail_host,25)
+    smtpObj.login(sender,password)
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException as e:
+    print("Error: 无法发送邮件",e)
+
+
